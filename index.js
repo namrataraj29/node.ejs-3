158ae1ebc3e5ae0108c4a3a71ef9ec7233022c07
namratraj29 <namrataraj2926@.com>
2024-04-16 16:03:47 +0530

    added

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require("express")
 const app = express()
+const path = require("path")
+
  const port = 8080
+
+ app.use(express.static("public"))
 
  app.set("view engine", "ejs")
  app.set("views", path.join(__dirname,"/views"))
  
  app.get("/",(req,res)=>{
     res.render("home.ejs")
+ })
+
+ app.get("/ig/:udername",(req,res)=>{
+   const followers = ["adam", "nam", "milky"]
+   let {username} = req.params
+   res.render("ig.ejs",{username,followers})
+ })
+
+
+
+ app.get("/hello",(req,res)=>{
+   res.send("hello")
  })
 
  app.get("/rolldice", (req,res)=>{
@@ -16,5 +32,8 @@ const app = express()
  app.listen(port,() =>{
     console.log(`listening on port ${port}`)
  })
+
+
+
 
 
